4c0f5e9a30d92d1d84154fe47f581ef6261611eb
Abzal <serikbay.a04@gmail.com>
2024-05-28 17:42:57 +0500

    tutorial_2: functions and control structures

---
+++ cmd/tutorial_2/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"errors"
+)
+
+func main() {
+	var printValue string = "Hello World!"
+	printMe(printValue)
+
+	var numerator int = 11
+	var dominator int = 2
+	var result, reminder, err = intDivision(numerator, dominator)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else if reminder == 0 {
+	// 	fmt.Printf("The result of integer devision is %v", result)
+	// } else {
+	// 	fmt.Printf("The result of integer devision is %v with reminder %v", result, reminder)
+	// }
+	switch {
+	case err != nil:
+		fmt.Println(err)
+	case reminder == 0:
+		fmt.Printf("The result of integer devision is %v", result)
+	default:
+		fmt.Printf("The result of integer devision is %v with reminder %v", result, reminder)
+	}
+
+	switch reminder {
+	case 0:
+		fmt.Println("The division was exact")
+	case 1, 2:
+		fmt.Println("The division was close")
+	default:
+		fmt.Println("The division was not close")
+	}
+
+}
+
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
+
+func intDivision(numerator int, dominator int) (int, int, error) {
+	var err error
+	if dominator == 0 {
+		err = errors.New("CannotDivideByZero")
+		return 0, 0, err
+	}
+	var result int = numerator / dominator
+	var reminder int = numerator % dominator
+	return result, reminder, err
+}
